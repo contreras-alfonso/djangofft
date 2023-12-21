bacb94814b565086f9ee8ebddf70b9cc7fa55b54
Alfonso Contreras <contrerasalfonso.lino@gmail.com>
2023-12-21 18:39:01 -0500

    add requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-sy=*&)sez576-#o-an5aiz33oq&eq449(6%f$9ttlxb!_99xnp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG","False").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
